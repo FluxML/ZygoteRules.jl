6124ea3a9d48c2f3bcf1c736e8947c149dea16ab
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2020-10-09 14:51:14 +0100

    add first attempt of suporting Composite

--- src/adjoint.jl
+++ src/adjoint.jl
@@ -1,6 +1,6 @@
 using MacroTools
 using MacroTools: @q, combinedef
-using ChainRulesCore: AbstractZero, Zero, DoesNotExist
+using ChainRulesCore: AbstractZero, Zero, DoesNotExist, Composite, unthunk
 
 named(arg) = isexpr(arg, :(::)) && length(arg.args) == 1 ? :($(gensym())::$(arg.args[1])) : arg
 
@@ -17,8 +17,8 @@ deleted once/if `@adjoint` macro is deprecated.
 function legacytype_warn()
   # can't use logging macros as that breaks nested AD.
   Core.println("""
-    Zygote internal use  of 'nothing', rather than `AbstractZero`, detected.
-    This should never occur. Please open an issue on
+    Zygote internal use of Nothing/Tuple/NamedTuple, rather than AbstractZero/Composite,
+    detected. This should never occur. Please open an issue on
     https://github.com/FluxML/Zygote.jl/issues, including the full text of this message.
     Stacktrace:"""
   )
@@ -37,8 +37,9 @@ deleted once/if `@adjoint` macro is deprecated.
 function difftype_warn()
   # can't use logging macros as that breaks nested AD.
   Core.println("""
-    AbstractZero passed when Nothing expected. Please open an issue on
-    https://github.com/FluxML/Zygote.jl/issues, including the full text of this message.
+    AbstractZero/Composite passed when NothingTuple/NamedTuple expected. This should never
+    occur. Please open an issue on https://github.com/FluxML/Zygote.jl/issues, including
+    the full text of this message.
     Stacktrace:"""
   )
   for (i, callsite) in enumerate(stacktrace())
@@ -53,24 +54,41 @@ Convert input `x` from the legacy ZygoteRules format to the ChainRules different
 """
 legacy2differential(x) = x
 legacy2differential(::Nothing) = Zero()
-legacy2differential(x::AbstractZero) = (difftype_warn(); return x)
-legacy2differential(t::Union{Tuple, NamedTuple}) = map(legacy2differential, t)
+legacy2differential(x::Union{AbstractZero, Composite}) = (difftype_warn(); return x)
+legacy2differential(t::Union{Tuple, NamedTuple}) = map(l2d, t)
 
+l2d(x) = x
+l2d(::Nothing) = Zero()
+function l2d(t::Union{Tuple, NamedTuple})
+  tp = map(l2d, t)
+  return Composite{Any, typeof(tp)}(tp)
+end
 """
     differential2legacy(x)
 
 Convert input `x` from the ChainRules differential types to the legacy ZygoteRules format.
 """
-differential2legacy(x) = x
+differential2legacy(x) = unthunk(x) # TODO eventually remove this
 differential2legacy(::AbstractZero) = nothing
 differential2legacy(t::Union{Tuple, NamedTuple}) = map(differential2legacy, t)
 differential2legacy(::Nothing) = (legacytype_warn(); return nothing)
+#differential2legacy(x::Tuple{Vararg{AbstractZero}}) = Zero() # TODO should this happen?
+for T_outer in (:Tuple, :NamedTuple)
+  # we create separate methods rather than using a `Union` + an `if` so that we avoid a
+  # branch that changes output type, because nested AD on that kinda thing makes Zygote less
+  # than happy.
+  @eval @inline function differential2legacy(x::Composite{P, T}) where {P, T<:$T_outer}
+    xp = map(differential2legacy, x)
+    convert($T_outer, xp)
+  end
+end
 
 for n = 0:3
   gradtuple = Symbol(:gradtuple, n)
   @eval begin
     $gradtuple(x::Tuple) = ($(ntuple(_->:(DoesNotExist()),n)...), x...)
     $gradtuple(x::AbstractZero) = x
+    $gradtuple(x::Composite) = x # TODO should this be here?
     $gradtuple(x) = error("Gradient $x should be a tuple")
   end
 end
