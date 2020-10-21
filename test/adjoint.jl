c4a2c6adc72a0e6303f775c5ae0727b449b176ba
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2020-10-21 10:50:30 +0100

    comment on tests

--- test/adjoint.jl
+++ test/adjoint.jl
@@ -2,23 +2,27 @@
     @test legacy2differential(nothing, Any) == Zero()
     @test legacy2differential(1, Any) == 1
 
+    # test gradient of a tuple
     legacy = (nothing, 1, nothing)
     differential = Composite{typeof(legacy)}(Zero(), 1, Zero())
-    @test legacy2differential(tuple(legacy), tuple(typeof(legacy))) == tuple(differential)
+    @test legacy2differential(tuple(legacy), typeof.(tuple(legacy))) == tuple(differential)
 
+    # test gradient of a named tuple
     legacy = (a=nothing, b=1, c=nothing)
     differential = Composite{typeof(legacy)}(a=Zero(), b=1, c=Zero())
-    @test legacy2differential(tuple(legacy), tuple(typeof(legacy))) == tuple(differential)
+    @test legacy2differential(tuple(legacy), typeof.(tuple(legacy))) == tuple(differential)
 
+    # test gradient of a struct
     struct Foo
         a
         b
     end
     legacy = (a=1, b=nothing)
     differential = Composite{Foo}(a=1, b=Zero())
-    @test legacy2differential(tuple(legacy), tuple(typeof(legacy))) == tuple(differential) # make this work and try as kwargs
+    @test legacy2differential(tuple(legacy), typeof.(tuple(legacy))) == tuple(differential) # this should fail once ChainRules newer version is used
     @test legacy2differential(tuple(legacy), tuple(Foo)) == tuple(differential)
 
+    # test gradient of a nested struct
     struct Bar
         x
         y::Foo
