5f2d7a87211bdb567ddf904dcea65eb0afe652c6
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2020-10-21 10:45:53 +0100

    Revert "turn legacy2differential to accepting kwargs"
    This reverts commit 3a76d339a3322c8af864399a49981fb5c95d006b.

--- test/adjoint.jl
+++ test/adjoint.jl
@@ -1,14 +1,14 @@
 @testset "legacy2differential" begin
-    @test legacy2differential(nothing) == Zero()
-    @test legacy2differential(1) == 1
+    @test legacy2differential(nothing, Any) == Zero()
+    @test legacy2differential(1, Any) == 1
 
     legacy = (nothing, 1, nothing)
     differential = Composite{typeof(legacy)}(Zero(), 1, Zero())
-    @test legacy2differential(tuple(legacy)) == tuple(differential)
+    @test legacy2differential(tuple(legacy), tuple(typeof(legacy))) == tuple(differential)
 
     legacy = (a=nothing, b=1, c=nothing)
     differential = Composite{typeof(legacy)}(a=Zero(), b=1, c=Zero())
-    @test legacy2differential(tuple(legacy)) == tuple(differential)
+    @test legacy2differential(tuple(legacy), tuple(typeof(legacy))) == tuple(differential)
 
     struct Foo
         a
@@ -16,8 +16,7 @@
     end
     legacy = (a=1, b=nothing)
     differential = Composite{Foo}(a=1, b=Zero())
-    @test legacy2differential(tuple(legacy)) == tuple(differential)
-    @test legacy2differential(tuple(legacy), tuple(typeof(legacy))) == tuple(differential)
+    @test legacy2differential(tuple(legacy), tuple(typeof(legacy))) == tuple(differential) # make this work and try as kwargs
     @test legacy2differential(tuple(legacy), tuple(Foo)) == tuple(differential)
 
     struct Bar
